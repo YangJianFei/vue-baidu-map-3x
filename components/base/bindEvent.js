5d09a689b5825fe6402a46cdf7abf2134df723e7
yangjianfei <yangjianfei@reconova.cn>
2022-03-25 15:31:48 +0800

    🐞 fix: 修改maker点击失效

--- components/base/bindEvent.js
+++ components/base/bindEvent.js
@@ -4,8 +4,8 @@ export default function (instance, eventList) {
   const ev = eventList || events[this.$options.name]
   ev && ev.forEach(event => {
     const hasOn = event.slice(0, 2) === 'on'
-    const eventName = hasOn ? event.slice(2) : event
-    const listener = this.$attrs[eventName]
-    listener && instance.addEventListener(event, listener.fns)
+    let eventName = hasOn ? event.slice(2) : event
+    const listener = this.$attrs['on' + eventName.replace(eventName[0], eventName[0].toUpperCase())]
+    listener && instance.addEventListener(event, listener)
   })
 }
